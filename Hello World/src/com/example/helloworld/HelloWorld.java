bd029a3c156b3f239bff446f61294d2eeac2e938
Yuri Lima <Yuri Lima>
2017-05-17 11:04:34 -0300

    Hello World in Java

---
+++ Hello World/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Yuri on 17/05/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("testando");
+    }
+}
